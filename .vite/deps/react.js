4432584629dffa30cc1a73e4630c0530eac1dee2
Olya Dubodel <dubodel.olya@gmail.com>
2025-12-21 06:38:53 +0300

    feat: add ui button

---
+++ .vite/deps/react.js
@@ -0,0 +1,2 @@
+import { require_react } from './chunk-O5RE5CKT.js';
+export default require_react();
--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -1,9 +1,16 @@
+import { Button } from '@shared/ui/Button';
+
 import styles from './Header.module.scss';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="container">Posts App Header</div>
+      <div className="container">
+        <div className={styles.content}>
+          <p>Posts App Header</p>
+          <Button>About Project</Button>
+        </div>
+      </div>
     </header>
   );
 };
