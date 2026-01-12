3eba0d96060f3ee7f4a3666381c87a09e5ab9ff3
Olya Dubodel <dubodel.olya@gmail.com>
2026-01-12 22:47:19 +0300

    feat: add link to the post card

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,13 +1,18 @@
 import type { Post } from '@features/PostList';
 import { CommentList } from '@widgets/CommentList';
 import { memo } from 'react';
+import { Link } from 'react-router';
 
 import styles from './PostCard.module.scss';
 
 export const PostCard = memo(function PostCard({ id, title, body }: Post) {
   return (
     <article className={styles.card}>
-      <h3 className={styles.title}>{title}</h3>
+      <h3 className={styles.title}>
+        <Link to={`/posts/${id}`} className={styles.link}>
+          {title}
+        </Link>
+      </h3>
       <p className={styles.description}>{body}</p>
       <CommentList postId={id} />
     </article>
