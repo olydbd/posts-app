6ecf224800e25cffc1fffb88ecf3c3d999eb0d95
Olya Dubodel <dubodel.olya@gmail.com>
2026-01-20 02:06:13 +0300

    feat: update footer

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,9 +1,45 @@
+import { Flower2, Heart } from 'lucide-react';
+import { Link } from 'react-router';
+
 import styles from './Footer.module.scss';
+
+const year = new Date().getFullYear();
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className="container">Posts App Footer</div>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <div className={styles.brand}>
+            <div className={styles.logo}>
+              <Flower2 className={styles.logoIcon} />
+            </div>
+            <span className={styles.logoText}>Blossom Posts</span>
+          </div>
+
+          <nav className={styles.nav}>
+            <Link to="#" className={styles.link}>
+              Privacy
+            </Link>
+            <Link to="#" className={styles.link}>
+              Terms
+            </Link>
+            <Link to="#" className={styles.link}>
+              Contact
+            </Link>
+          </nav>
+
+          <p className={styles.made}>
+            Made with
+            <Heart className={styles.iconHeart} />
+            by the community
+          </p>
+        </div>
+
+        <div className={styles.bottom}>
+          <p>© {year} Blossom Posts. All rights reserved.</p>
+        </div>
+      </div>
     </footer>
   );
 };
