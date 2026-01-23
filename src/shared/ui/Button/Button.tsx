a0fad9486d32dd53e002e1307bc9202bf4781372
Olya Dubodel <dubodel.olya@gmail.com>
2026-01-23 21:13:42 +0300

    refactor: use PropsWithChildren, MouseEventHandler

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,14 +1,13 @@
-import type { ReactNode } from 'react';
+import type { MouseEventHandler, PropsWithChildren } from 'react';
 
 import styles from './Button.module.scss';
 
 interface ButtonProps {
-  children: ReactNode;
-  onClick?: () => void;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
 }
 
-export const Button = ({ children, onClick, className }: ButtonProps) => {
+export const Button = ({ children, onClick, className }: PropsWithChildren<ButtonProps>) => {
   return (
     <button type="button" onClick={onClick} className={`${styles.button} ${className}`}>
       {children}
